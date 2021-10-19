7b2e3e1ef58914869d983d7310d7b043043b18f9
曾俊刚 <zjgang1@grgbanking.com>
2021-10-19 21:36:29 +0800

    更改

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ http
 
       // 拉取仓库最新代码
       //github.com/webdayZj/Amap-.git
+      // git  第二次
       // git clone https://github.com/yeyan1996/${data.repository.name}.git ${projectDir}
       execSync(
         `git clone https://github.com/webdayZj/${data.repository.name}.git ${projectDir}`,
